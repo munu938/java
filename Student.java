7ddb71fbe77369f07322bc783574a2f4f9ac4929
Apurba Choudhury <choudhuryapurba098@gmail.com>
2025-07-24 11:44:00 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,25 @@
+package day_3;
+
+
+public class Student{
+
+	
+public	String  name  ;
+	
+public	String address  ;
+	
+public	int marks  ;
+
+	public void  study() {
+		System.out.println(" Yes, i am doing study and  also playing basketball as sports.  ");
+	}
+	
+	
+	public void displayStudentData() {
+	System.out.println("Name is " +name );	
+	System.out.println("Address is "+ address);
+	System.out.println("Marks is " + marks );
+	}
+}
+
+
