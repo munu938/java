afe64a4bfd6ebf08690d28a940d6e8b66821cec1
Apurba Choudhury <choudhuryapurba098@gmail.com>
2025-07-24 11:48:12 +0530

    Add files via upload

---
+++ Childchild.java
@@ -0,0 +1,15 @@
+package day_3;
+ import day.Basic_info;
+public class Childchild {
+	public static void main(String[] args) {
+		
+		Childchild deva = new Childchild();
+		double total_property= deva.total_property;
+
+		String s = deva.class_name() ;
+		System.out.println("total_property");
+		
+		
+	}
+
+}
