2de58acd9be0f9a97678eaec9df913394d1639a6
Apurba Choudhury <choudhuryapurba098@gmail.com>
2025-07-24 11:47:54 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,18 @@
+package day_3;
+
+public class Demo {
+	public static void main(String[] args) {
+	
+		Employee e1 = new Employee() ;
+		
+		e1.employee_name="amit";
+		e1.setemployee_name("raju");
+		 System.out.println(e1.getemployee_name());
+	
+	   System.out.println(e1.getemployee_name());
+	
+	
+	
+	}
+
+}
