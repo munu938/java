53fdf399b9423a5da8ca21280f733825656b666f
Apurba Choudhury <choudhuryapurba098@gmail.com>
2025-07-24 11:31:11 +0530

    Add files via upload

---
+++ Basic_info.java
@@ -0,0 +1,62 @@
+package day;
+
+//this is used for one line comment
+
+
+
+//ctrl+
+
+/*assf
+dnsdjdsjk
+ajbdv*/
+
+public class Basic_info {
+	
+ public String class_name  = " Surya ";
+	 
+	 public String Safe_password = "Surya@123" ;
+	 
+	 
+	 protected double total_property  = 120033323.89 ;
+	 
+	 
+	
+	//allColoured words are keywords public class package 
+	
+	//Method bananas key process but here method means set of lines which are doing something 
+	
+	//In built method mean 
+	//IDE an integrated development environment is a software application that helps programmer develop software code efficiently 
+	
+	//main-->ctrl+space
+	
+	//main method-----
+
+		//it is the entry point for any java program
+
+	//syso+ctrl+space
+		
+	//	System.out.println("Today is our first day ");
+		
+		//camelcase
+		//pascalcase
+	
+		//variable 
+		// datatype
+	 public static void main(String[] args) {
+	 
+		 int age=20;
+		  char s='s';
+		  boolean bool= true;
+		  String name ="rohi";
+		  String aadhar="234u723789574";//
+		  String pan="wtyyl356467676";
+		  
+		  System.out.println("rohi's aadhar"+ aadhar);
+  int a =20;
+ integer k=20;
+	 }
+public void myMethod() {
+	
+}
+}
