4c68be9831c6c34ddca0c1a7500bd1c38018f67a
Apurba Choudhury <choudhuryapurba098@gmail.com>
2025-07-28 19:22:30 +0530

    Add files via upload

---
+++ NumIncrease.java
@@ -0,0 +1,13 @@
+package day_4;
+
+public class NumIncrease {
+public static void main(String[] args) {
+	int i,j,n=6;
+	for(i=1;i<=n;i++) {
+		for(j=1;j<=i;j++) {
+			System.out.print(j+"");
+		}
+		System.out.println();
+	}
+}
+}
