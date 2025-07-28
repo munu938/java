ddac17d9c6c1537b78680c12ba2612b9f0a86b0b
Apurba Choudhury <choudhuryapurba098@gmail.com>
2025-07-28 19:23:34 +0530

    Add files via upload

---
+++ Numtri.java
@@ -0,0 +1,16 @@
+package day_4;
+import java.util.*;
+public class Numtri {
+public static void main(String[] args) {
+	int i,j,n=9;
+	for(i=1;i<= n;i++) {
+		for(j=1;j<=n-i;j++){
+			System.out.print("");
+		}
+		for(j=1;j<=i;j++) {
+			System.out.print(i+"");
+		}
+		System.out.println();
+	}
+}
+}
