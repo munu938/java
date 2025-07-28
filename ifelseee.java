4af147f580c46b2273ff005a8ef0d1a20f96f41f
Apurba Choudhury <choudhuryapurba098@gmail.com>
2025-07-28 19:21:16 +0530

    Add files via upload

---
+++ ifelseee.java
@@ -0,0 +1,35 @@
+package day_4;
+
+public class ifelseee {
+	public static void main(String[] args) {
+
+
+int age=12;
+if (age>-10) {
+	System.out.println("aap vote de sakte ho");
+}else {
+	System.out.println("you are not eligible");
+}
+
+int marks=86;
+//and and or operator
+if (marks>90) {
+	System.out.println("A");
+}else if(marks>8 && marks <=70) {
+	System.out.println("special case");
+}else {
+	System.out.println("c");
+}
+int a=20;
+int b=25;
+//if(a-20 || b-30) {
+	System.out.println("both correct");
+//}
+if(a==25 && b==25) {
+	System.out.println("true");
+	
+}else {
+	System.out.println("false");
+}
+}
+}
