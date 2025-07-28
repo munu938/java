cbf7bd989f7f93952f1de8c224217609ca512b3e
Apurba Choudhury <choudhuryapurba098@gmail.com>
2025-07-28 19:22:53 +0530

    Add files via upload

---
+++ Numinre.java
@@ -0,0 +1,14 @@
+package day_4;
+
+public class Numinre {
+public static void main(String[] args) {
+	int i,j,n=9;
+	
+	for(i=n; i>=1; i--) {
+		for(j=1;j<=i;j++) {
+			System.out.print(j+"");
+		}
+		System.out.println();
+	}
+}
+}
